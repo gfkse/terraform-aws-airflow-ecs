b0eee653dde3d729e3fccc60e7161050bf27ace4
AzureAD\IkerváriRóbert <ikervari@marktsoft.hu>
2021-12-15 14:33:35 +0100

    #17 configure DAG sync transfer mode and preserve files behavior

--- data_sync_dags.tf
+++ data_sync_dags.tf
@@ -25,7 +25,9 @@ resource "aws_datasync_task" "dag_sync" {
   source_location_arn      = aws_datasync_location_s3.dag_source.arn
 
   options {
-    bytes_per_second = -1
-    verify_mode      = "ONLY_FILES_TRANSFERRED"
+    bytes_per_second       = -1
+    transfer_mode          = "ALL"
+    verify_mode            = "ONLY_FILES_TRANSFERRED"
+    preserve_deleted_files = "REMOVE"
   }
 }
